6c75f8452361bf3d1b375d93c9dfc3af03804c93
jwang <mac@macmatoMacBook-Air.local>
2014-11-02 17:14:39 +0800

    form and js hide/show

--- app_my_restaurant/models.py
+++ app_my_restaurant/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from django.forms import ModelForm
+from django.contrib.auth.models import User
 
 # Create your models here.
+class my_restaurant_info_model(models.Model):
+    user = models.OneToOneField(User)
+    restaurant_name = models.CharField(max_length=100, default="Here Your Restaurant Name")
+    restaurant_description = models.TextField(default="Here Your Restaurant Desciption")
+    pass
 
-#class my_restaurant_info_form(ModelForm):
-#    # TODO form for restaurant info
+
+class my_restaurant_info_form(ModelForm):
+    # TODO form for restaurant info
 #    restaurant_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),
 #        max_length=100,
 #        required=True,
 #        help_text='Please set your restaurant name')
-#    pass
+    class Meta:
+        model = my_restaurant_info_model
+    pass
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,7 @@ from app_my_restaurant.views import my_restaurant_home
 from app_my_restaurant.views import my_restaurant_info
 from app_my_restaurant.views import my_restaurant_menu
 from app_my_restaurant.views import my_restaurant_desk
+from app_my_restaurant.views import my_restaurant_update_name
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -29,6 +30,8 @@ urlpatterns = patterns('',
     url(r'^my_restaurant/info$', my_restaurant_info, name="my_restaurant_info"),
     url(r'^my_restaurant/menu$', my_restaurant_menu, name="my_restaurant_menu"),
     url(r'^my_restaurant/desk$', my_restaurant_desk, name="my_restaurant_desk"),
+
+    url(r'^my_restaurant/info/name_update$', my_restaurant_update_name, name="my_restaurant_update_name"),
 
     (r'^resetpassword/passwordsent/$','django.contrib.auth.views.password_reset_done'),
     (r'^resetpassword/$','django.contrib.auth.views.password_reset'),
