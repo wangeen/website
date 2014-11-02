0a2a2398c35b2805d14112d86a3997e0e3a6a9a7
jwang <mac@macmatoMacBook-Air.local>
2014-11-02 12:00:13 +0800

    my restaurant edit

--- app_my_restaurant/models.py
+++ app_my_restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.forms import ModelForm
 
 # Create your models here.
+
+#class my_restaurant_info_form(ModelForm):
+#    # TODO form for restaurant info
+#    restaurant_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),
+#        max_length=100,
+#        required=True,
+#        help_text='Please set your restaurant name')
+#    pass
