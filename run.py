a48d7012626d02cf127a869cdfce64c908707e55
wangeen_aliyun <wangeen@qq.com>
2014-11-14 09:33:58 +0800

    wsgi

--- run.py
+++ run.py
@@ -9,15 +9,22 @@ def script(cmd):
 
 
 if __name__  == "__main__":
-    script("python manage.py runserver")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d",  "--develop_server",  help="django default develop server", action='store_true')
+    parser.add_argument("-u",  "--uwsgi_server",  help="uwsgi server", action='store_true')
+
+    args = parser.parse_args()
+    if args.develop_server:
+        script("python manage.py runserver")
+    elif args.uwsgi_server:
+        script("uwsgi --http :8000 --chdir /root/website --module django_wsgi")
+    else:
+        parser.print_help()
 
 
 
 
-#    parser = argparse.ArgumentParser()
-#    parser.add_argument("-m",  "--message",  help="this is sample python script", action='store_true')
 #    parser.add_argument("-r",  "--run",  help="run local server", action='store_true')
-#    args = parser.parse_args()
 #    if args.message:
 #        print "start script"
 #        ## TODO,  to be added here
